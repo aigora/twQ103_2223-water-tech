632c1ab9159b7822ebad2b6c090b517a63bb298b
itziarsg <102077512+itziarsg@users.noreply.github.com>
2023-04-28 21:06:43 +0200

    YA COMPILA EL PROGRAMA E IMPRIME LOS FICHEROS
    también he cambiado el nombre de los ficheros como pide la profe. El único problema que ay esque no se como poner los tabuladores para que el fichero se imprima bonito.

--- TRABAJO INFO.cpp
+++ TRABAJO INFO.cpp
@@ -10,61 +10,109 @@ struct Taguas{
 	float coliformes;
 };
 
+void mostrarMenu(int);
+void abrirFichero(char[]);
+
 int main(){
 	setlocale(LC_CTYPE,("Spanish"));
 	int opcion;
-	char nombreArchivo [50];
+	char nombreFichero [50];
 	FILE*fichero;
+	char fichero01[] = "202301_Arroyomolinos.txt";
+    char fichero02[] = "202301_Lavapiés.txt";
+    char fichero03[] = "202301_Leganés.txt";
+    char fichero04[] = "202301_Usera.txt";
+    char fichero05[] = "202301_Vicálvaro.txt";
 	
-	printf("-----------WATER TECH------------\n");
-	printf("Bienvenido a Water Tech, una aplicación diseñada para ayudarte en el registro y análisis de los datos de calidad del agua en distintas fuentes de Madrid. Con esta herramienta, podrás cargar y almacenar ficheros de datos mensuales, realizar estadísticas y búsquedas sobre los datos, y mucho más.Esperamos que disfrutes utilizando Water Tech y que te sea de gran ayuda en tu investigación sobre la calidad del agua. Si tienes alguna pregunta o sugerencia, no dudes en contactarnos.¡Gracias por elegir Water Tech!\n");
-}
-	 /*do {
-    	printf("\nSeleccione una opción:\n");
-        printf("1. Cargar fichero de datos\n");
-        printf("2. Mostrar datos de una fuente\n");
-        printf("3. Buscar fuentes por parámetro\n");
-        printf("4. Añadir datos de una fuente\n");
-        printf("5. Salir\n");
+	printf("--------------------------------------------------------------- WATER TECH ----------------------------------------------------------------------------\n");
+	printf("\nBienvenido a Water Tech, una aplicación diseñada para ayudarte en el registro y análisis de los datos de calidad del agua en distintas fuentes de Madrid en el mes de Enero en 2023.\nCon esta herramienta, podrás cargar y almacenar ficheros de datos mensuales, realizar estadísticas y búsquedas sobre los datos, y mucho más.\nEsperamos que disfrutes utilizando Water Tech y que te sea de gran ayuda en tu investigación sobre la calidad del agua.\nSi tienes alguna pregunta o sugerencia, no dudes en contactarnos.¡Gracias por elegir Water Tech!\n");
+	
+	do {
+        mostrarMenu(opcion);
         scanf("%d", &opcion);
 
-	switch(opcion) {
-            case '1': 
-            	// PASO 1: ABRIR EL ARCHIVO:
-            	
-            	printf("\nIntroduce el nombre del archivo:\n");
-            	scanf("%s", nombreArchivo);
-            
-            	fichero=fopen ("fichero de trabajo.txt",'r'); 
-            	
-            	if (fichero==NULL){
-            		printf ("ERROR, no se puede abrir el archivo: %s\n", nombreArchivo);
-            		return 0;
-				}
-				
-				//PASO 2: LEER EL ARCHIVO 
-				
-				while(fscanf(fichero, "%s %f %f %f %f", nombreFuente, &ph, &conductividad, &turbidez, &coliformes)!=EOF){
-					printf("%s %f %f %f %f\n", nombreFuente, ph, conductividad, turbidez, coliformes);
-				}
-				
-				// PASO 3: CERRAR EL ARCHIVO:
-				fclose (fichero);
-            
-            	break;
-            case '2':
+        switch(opcion) {
+            case 1:
+                printf("\nHa seleccionado la opción 1\n");
+                abrirFichero(fichero01);
                 break;
-            case '3':
-            	 break;
-            case '4':
+            case 2:
+                printf("\nHa seleccionado la opción 2\n");
+                abrirFichero(fichero02);
                 break;
-            case '5':
+            case 3:
+                printf("\nHa seleccionado la opción 3\n");
+                abrirFichero(fichero03);
                 break;
+            case 4:
+                printf("\nHa seleccionado la opción 4\n");
+                abrirFichero(fichero04);
+                break;
+            case 5:
+                printf("\nHa seleccionado la opción 5\n");
+                abrirFichero(fichero05);
+                break;
+            case 6: 
+            	printf("\nHa seleccionado la opción 6. Saliendo del programa...\n");
+            	return 0;
             default:
-                printf("\nOpción inválida, por favor seleccione una opción válida.\n");
+                printf("\nOpción no válida. Por favor, seleccione una opción válida.\n");
+                break;
         }
-    } while(opcion != '5');
+    } while(opcion < 1 || opcion > 5);
 
     return 0;
-}; */
+		
+};
+
+// FUNCIONES 
+
+void mostrarMenu(int opcion) {
+    printf("\nSeleccione fichero de datos desea cargar:\n");
+    printf("1: Datos fuentes de Arroyomolinos en 01/2023\n");
+    printf("2: Datos fuentes de Lavapiés en 01/2023 \n");
+    printf("3: Datos fuentes de Leganés en 01/2023\n");
+    printf("4: Datos fuentes de Usera en 01/2023\n");
+    printf("5: Datos fuentes de Vicálvaro en 01/2023\n");
+    printf("6: Salir de Water Tech\n");
+    
+    return;
+};
+
+void abrirFichero(char nombreFichero[]) {
+    // Paso 1:abrir archivo
+	FILE *fichero;
+    fichero = fopen(nombreFichero, "r");
+    if (fichero == NULL) {
+        printf("\nError al abrir el archivo\n");
+	}else {
+	//Paso 2: leer archivo
+        printf("\nArchivo abierto correctamente.\n");
+        printf("Parametros\t\tpH\tConductividad\tTurbidez\tColiformes\n");
+
+        struct Taguas tagua;
+        while (fscanf(fichero, "%s %f %f %f %f", tagua.nombreFuente, &tagua.ph, &tagua.conductividad, &tagua.turbidez, &tagua.coliformes) != EOF) {
+            printf("\n %s\t%.2f\t%.0f\t%.0f\t%.0f\n", tagua.nombreFuente, tagua.ph, tagua.conductividad, tagua.turbidez, tagua.coliformes);
+        }
+        printf("\n");
+        
+    // Paso 3:cerrar archivo
+        fclose(fichero);
+    }
+    return;
+}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
 
