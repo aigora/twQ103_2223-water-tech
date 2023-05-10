0023817d4621a96e121e2bdc0720e1499ef0ad70
itziarsg <102077512+itziarsg@users.noreply.github.com>
2023-05-10 12:41:40 +0200

    pasado a .c

--- TRABAJO INFO.c
+++ TRABAJO INFO.c
@@ -2,9 +2,8 @@
 #include <string.h>
 #include <locale.h>
 
-
 struct Taguas{
-	char nombreFuente [50];
+	char nombreFuente [100];
 	float ph;
 	float conductividad;
 	float turbidez;
@@ -12,159 +11,108 @@ struct Taguas{
 };
 
 void mostrarMenu(int);
+void abrirFichero(char[]);
 
-int main () {
+int main(){
 	setlocale(LC_CTYPE,("Spanish"));
-
+	int opcion;
+	char nombreFichero [50];
+	FILE*fichero;
+	char fichero01[] = "202301_Arroyomolinos.txt";
+    char fichero02[] = "202301_Lavapiés.txt";
+    char fichero03[] = "202301_Leganés.txt";
+    char fichero04[] = "202301_Usera.txt";
+    char fichero05[] = "202301_Vicálvaro.txt";
 	
-	FILE * pfichero;
-
-	int Municipio, opcion, Mes;
-	char aux;
+	printf("--------------------------------------------------------------- WATER TECH ----------------------------------------------------------------------------\n");
+	printf("\nBienvenido a Water Tech, una aplicación diseñada para ayudarte en el registro y análisis de los datos de calidad del agua en distintas fuentes de Madrid en el mes de Enero en 2023.\nCon esta herramienta, podrás cargar y almacenar ficheros de datos mensuales, realizar estadísticas y búsquedas sobre los datos, y mucho más.\nEsperamos que disfrutes utilizando Water Tech y que te sea de gran ayuda en tu investigación sobre la calidad del agua.\nSi tienes alguna pregunta o sugerencia, no dudes en contactarnos.¡Gracias por elegir Water Tech!\n");
 	
-	printf("-----------WATER TECH------------\n");
-	printf("Bienvenido a Water Tech, una aplicación diseñada para ayudarte en el registro y análisis de los datos de calidad del agua en distintas fuentes de Madrid. Con esta herramienta, podrás cargar y almacenar ficheros de datos mensuales, realizar estadísticas y búsquedas sobre los datos, y mucho más. Esperamos que disfrutes utilizando Water Tech y que te sea de gran ayuda en tu investigación sobre la calidad del agua. Si tienes alguna pregunta o sugerencia, no dudes en contactarnos. ¡Gracias por elegir Water Tech!\n");
-	
-	while (1) {
-		
-		printf ("Introduzca número el barrio/Municipio que quiera:\n1.Arroyomolinos.\n2.Usera.\n3.Leganés.\n4.Lavapiés.\n");
-		scanf ("%d", &Municipio);
-		
-		if (Municipio < 1 || Municipio > 4) {
-			printf ("Lo siento, pero tiene que introducir un número de los municipios dados.\n");
-		} else {
-			break;
-		}
-	}
-	
-	switch (Municipio) {
-		case 1:
-			mostrarMenu(opcion);
-			scanf ("%d", &opcion);
-			
-			switch (opcion) {
-				case 1:	 
-			//Opción 1: Ver las fuentes del barrio según el més.
-				while (1) {
-					printf ("De que mes desea sacar los datos de la fuentes de %s, siguiendo el formato 01,02,03...\n", Municipio);
-					scanf ("%d", Mes);
-				
-					if (Mes != 1 && Mes != 2) {
-						printf ("Solo disponemos de los dos primeros meses.\n");
-					} else {
-						break;
-					}
-				}	
-			
-				if (Mes == 01) {
-			
-				pfichero = fopen ("202301_Arroyomolinos.txt","r"); //Abrir el fichero.
-					if (pfichero == NULL) {
-						printf ("No se ha podido abrir el fichero.\n");
-						return 0;
-					}
-		
-					while (aux != EOF) {
-						aux = fgetc(pfichero);
-						printf ("%c", aux);
-					}
-				fclose(pfichero);
-				}
-				if (Mes == 02) {
-			
-				pfichero = fopen ("202302_Arroyomolinos.txt","r"); //Abrir el fichero.
-					if (pfichero == NULL) {
-						printf ("No se ha podido abrir el fichero.\n");
-						return 0;
-					}
-		
-					while (aux != EOF) {
-						aux = fgetc(pfichero);
-						printf ("%c", aux);
-					}
-				fclose(pfichero);	
-				}
-			//Fín de la opción 1.
+	do {
+        mostrarMenu(opcion);
+        scanf("%d", &opcion);
 
-		case 2:
-			//Opcion 2: Buscar fuentes por parámetros.		
-			mostrarMenu(opcion);
-			scanf ("%d", &opcion);
-			
-			//Opción 1: Ver las fuentes del barrio según el més.
-			while (1) {
-				printf ("De que mes desea sacar los datos de la fuentes de %s, siguiendo el formato 01,02,03...\n", Municipio);
-				scanf ("%d", Mes);
-				
-				if (Mes != 1 && Mes != 2) {
-					printf ("Solo disponemos de los dos primeros meses.\n");
-				} else {
-					break;
-				}
-			}
-			
-			if (Mes == 01) {
-			
-			pfichero = fopen ("202301_Usera.txt","r"); //Abrir el fichero.
-				if (pfichero == NULL) {
-					printf ("No se ha podido abrir el fichero.\n");
-					return 0;
-				}
+        switch(opcion) {
+            case 1:
+                printf("\nHa seleccionado la opción 1\n");
+                abrirFichero(fichero01);
+                break;
+            case 2:
+                printf("\nHa seleccionado la opción 2\n");
+                abrirFichero(fichero02);
+                break;
+            case 3:
+                printf("\nHa seleccionado la opción 3\n");
+                abrirFichero(fichero03);
+                break;
+            case 4:
+                printf("\nHa seleccionado la opción 4\n");
+                abrirFichero(fichero04);
+                break;
+            case 5:
+                printf("\nHa seleccionado la opción 5\n");
+                abrirFichero(fichero05);
+                break;
+            case 6: 
+            	printf("\nHa seleccionado la opción 6. Saliendo del programa...\n");
+            	return 0;
+            default:
+                printf("\nOpción no válida. Por favor, seleccione una opción válida.\n");
+                break;
+        }
+    } while(opcion < 1 || opcion > 5);
+
+    return 0;
 		
-				while (aux != EOF) {
-					aux = fgetc(pfichero);
-					printf ("%c", aux);
-				}
-			fclose(pfichero);
-			}
-			if (Mes == 02) {
-			
-			pfichero = fopen ("202302_Usera.txt","r"); //Abrir el fichero.
-				if (pfichero == NULL) {
-					printf ("No se ha podido abrir el fichero.\n");
-					return 0;
-					}
-		
-				while (aux != EOF) {
-					aux = fgetc(pfichero);
-					printf ("%c", aux);
-				}
-			fclose(pfichero);	
-			}
-			//Fín de la opción 1.
-			//Opcion 2: Buscar fuentes por parámetros.
-			
-		
-			break;
-		case 3:
-			mostrarMenu(opcion);
-			scanf ("%d", &opcion);
-			break;
-		case 4:
-			mostrarMenu(opcion);
-			scanf ("%d", &opcion);
-			break;
-	}
+};
 
-}
-
-	
-
-	
-	
-
+// FUNCIONES 
 
 void mostrarMenu(int opcion) {
-	
-	printf("\nSeleccione una opción:\n");
-    printf("1. Mostrar datos de una fuente\n"); //El fichero hay que pasarlo a una estructura.
-    printf("2. Buscar fuentes por parámetro\n"); //Igual que la opción dos.
-    printf("3. Añadir datos de una fuente\n");  // Craer un nuevo fichero.
-    printf("4. Ver la ubicación y el nombre de la fuente introducida.\n");
-	printf("5. Comprobar la potabilidad del agua.\n");
-	printf("6. Fuente más ácida y más básica\n");
-    printf("7. Salir\n");
+    printf("\nSeleccione fichero de datos desea cargar:\n");
+    printf("1: Datos fuentes de Arroyomolinos en 01/2023\n");
+    printf("2: Datos fuentes de Lavapiés en 01/2023 \n");
+    printf("3: Datos fuentes de Leganés en 01/2023\n");
+    printf("4: Datos fuentes de Usera en 01/2023\n");
+    printf("5: Datos fuentes de Vicálvaro en 01/2023\n");
+    printf("6: Salir de Water Tech\n");
     
     return;
 };
+
+void abrirFichero(struct Taguas vector[], int tam, char nombreFichero[]) {
+    FILE *fichero;
+    fichero = fopen(nombreFichero, "r");
+    if (fichero == NULL) {
+        printf("\nError al abrir el archivo\n");
+	}else {
+        printf("\nArchivo abierto correctamente.\n");
+        printf("Parametros\tpH\tConductividad\tTurbidez\tColiformes\n");
+
+        int i = 0;
+        while (fscanf(fichero, "%s %f %f %f %f", vector[i].nombreFuente, &vector[i].ph, &vector[i].conductividad, &vector[i].turbidez, &vector[i].coliformes) != EOF) {
+            printf("%s\t%.2f\t%.0f\t%.0f\t%.0f\n", vector[i].nombreFuente, vector[i].ph, vector[i].conductividad, vector[i].turbidez, vector[i].coliformes);
+            i++;
+            if (i >= tam) { // verificar que no se exceda el tamaño del vector
+                break;
+            }
+        }
+        fclose(fichero);
+    }
+    return;
+};
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
 
